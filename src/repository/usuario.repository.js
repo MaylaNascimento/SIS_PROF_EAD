98e217b6d510547d5baa7d9d5a71e040305d86c9
Gutemberg <saddam.gutem@gmail.com>
2022-12-07 12:32:28 -0300

    C5: Fixes

--- src/repository/usuario.repository.js
+++ src/repository/usuario.repository.js
@@ -5,7 +5,16 @@ async function insertUsuario(){
 }
 
 async function selectUsuario(){
-    
+    return new Promise((resolve, reject) =>{
+        database.get(
+            `select u.nome_completo, u.login, u.id_permissao, p.nome
+            from usuarios u join permissoes p on u.id_permissao = p.id`, 
+            (err, rows)=>{
+                if (err) reject(err);
+                if (rows.length == 0) reject(new Error('Usuario não encontrado'));
+                resolve(rows);
+        })
+    })
 }
 
 export {insertUsuario, selectUsuario}
